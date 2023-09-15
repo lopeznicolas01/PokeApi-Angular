484f5a9fe65515abc1cc39388f897eda73b656ae
GQuiroz <74075209+GabrielQuiroz1995@users.noreply.github.com>
2023-09-15 12:14:55 -0300

    prueba de formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioPokemon } from './formulario-pokemon.componen/formulario-pokemon.componen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioPokemon.ComponenComponent
   ],
   imports: [
     BrowserModule,
